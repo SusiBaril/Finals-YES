ed8b20e24d9ae7150e850502ab19716d7e779a5c
SusiBaril <rosaleskeegan@gmail.com>
2024-05-25 22:14:26 +0800

    New Changes

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ new Chart(ctxPie, {
     }
 });
 
+// Products Section
+
 const ctxBar = document.getElementById('barchart');
 
 new Chart(ctxBar, {
@@ -110,6 +112,79 @@ new Chart(ctxBar, {
                 position: 'bottom',
                 labels: {
                     color: 'white' // set legend text color to white
+                  }
+            }
+        }
+    }
+});
+
+// Sales Section
+
+const ctxLine1 = document.getElementById('linechart');
+
+new Chart(ctxLine1, {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+                label: 'Brand',
+                data: [3000, 2500, 3000, 5000, 4000, 3000, 3500],
+                borderColor: '#FF66C4',
+                borderWidth: 2
+            },
+            {
+                label: 'Tools',
+                data: [2000, 3500, 3500, 3000, 2000, 1500, 2400],
+                borderColor: '#A375FF',
+                borderWidth: 2
+            },
+            {
+                label: 'Stock',
+                data: [1000, 1500, 2000, 2500, 3000, 2000, 1000],
+                borderColor: '#FF914D',
+                borderWidth: 2
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    color: 'white' // set legend text color to white
+                  }
+            }
+        }
+    }
+});
+
+const ctxPie1 = document.getElementById('pieschart');
+
+new Chart(ctxPie1, {
+    type: 'doughnut',
+    data: {
+        labels: ['Car Accesories', 'Oil', 'Degreaser', 'Auto Parts', 'Fluid'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100, 50, 20],
+            backgroundColor: [
+                '#FF66C4',
+                '#A375FF',
+                '#FF914D',
+                '#FFBD59',
+                '#5271FF'
+            ],
+            hoverOffset: 4
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    color: 'white',
                   }
             }
         }
